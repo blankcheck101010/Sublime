8ff2de43b9b8e6fb3ba3d22f6fb7804a4a07830e
Saul Alvarez <saul@sauls-macbook.gateway.sonic.net>
2018-10-27 17:34:38 -0700

    updates to your macros and added vim toggle to pc in sublime

--- My Sublime Packages/MyPlugins/_ReadOnly.py
+++ My Sublime Packages/MyPlugins/_ReadOnly.py
@@ -1,24 +1,24 @@
-import sublime
-import sublime_plugin
+# import sublime
+# import sublime_plugin
 
 
 
-class ReadOnlyCommand(sublime_plugin.TextCommand):
+# class ReadOnlyCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit):
-        quit()
+#     def run(self, edit):
+#         quit()
 
-        if self.view.is_read_only():
-            setts = sublime.load_settings('Preferences.sublime-settings')
-            ignored = setts.get('ignored_packages')
+#         if self.view.is_read_only():
+#             setts = sublime.load_settings('Preferences.sublime-settings')
+#             ignored = setts.get('ignored_packages')
 
-            if 'NeoVintageous' in ignored:
-                ignored.remove('NeoVintageous')
-            else:
-                ignored.append('NeoVintageous')
+#             if 'NeoVintageous' in ignored:
+#                 ignored.remove('NeoVintageous')
+#             else:
+#                 ignored.append('NeoVintageous')
 
-            setts.set('ignored_packages', ignored)
-            sublime.save_settings('Preferences.sublime-settings')
+#             setts.set('ignored_packages', ignored)
+#             sublime.save_settings('Preferences.sublime-settings')
 
-            # sublime.message_dialog(ignored)
-            # sublime.message_dialog('I am')
+#             # sublime.message_dialog(ignored)
+#             # sublime.message_dialog('I am')
--- My Sublime Packages/MyPlugins/_ReadOnlyView.py
+++ My Sublime Packages/MyPlugins/_ReadOnlyView.py
@@ -1,13 +1,13 @@
-import sublime
-import sublime_plugin
+# import sublime
+# import sublime_plugin
 
 
 
-class ReadOnlyView(sublime_plugin.EventListener):
+# class ReadOnlyView(sublime_plugin.EventListener):
 
-    def on_activated(self, view):
-        if view.is_read_only():
-            quit()
-            # sublime.message_dialog('Hey thehhue, I am readonly :)')
+#     def on_activated(self, view):
+#         if view.is_read_only():
+#             quit()
+#             # sublime.message_dialog('Hey thehhue, I am readonly :)')
 
-            # sublime.message_dialog('Not readonly :(')
+#             # sublime.message_dialog('Not readonly :(')
